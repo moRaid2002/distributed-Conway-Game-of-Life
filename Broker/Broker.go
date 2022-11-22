3366c761dbd0431df62f6890cb0a34fee5a67e77
Mohamed Raid Athamena <jg20659@it075798.wks.bris.ac.uk>
2022-11-22 20:47:33 +0000

    message

--- Broker/Broker.go
+++ Broker/Broker.go
@@ -28,21 +28,26 @@ func (s *Broker) Client(req stubs.Request, res *stubs.Response) (err error) {
 	}
 	server := flag.String("server-1-"+strconv.Itoa(x), "44.201.122.214:8030", "IP:port string to connect to as server")
 	server2 := flag.String("server-2-"+strconv.Itoa(x), "54.90.210.50:8030", "IP:port string to connect to as server")
+	server3 := flag.String("server-3-"+strconv.Itoa(x), "18.212.57.66:8030", "IP:port string to connect to as server")
 	x++
 	flag.Parse()
 	client, _ := rpc.Dial("tcp", *server)
 	client2, _ := rpc.Dial("tcp", *server2)
+	client3, _ := rpc.Dial("tcp", *server3)
 	defer client.Close()
 	defer client2.Close()
 	newState := req.CurrentStates
 
 	for turns := 0; turns < req.P.Turns; turns++ {
-		request := stubs.Request{newState, req.P, 0, "", 2, 0}
-		request2 := stubs.Request{newState, req.P, 0, "", 2, req.P.ImageHeight / 2}
+		request := stubs.Request{newState, req.P, 0, "", 3, 0}
+		request2 := stubs.Request{newState, req.P, 0, "", 3, int(req.P.ImageHeight / 3)}
+		request3 := stubs.Request{newState, req.P, 0, "", 3, 2 * int(req.P.ImageHeight/3)}
 		response := new(stubs.Response)
 		response2 := new(stubs.Response)
+		response3 := new(stubs.Response)
 		makeCall(client, request, response)
 		makeCall(client2, request2, response2)
+		makeCall(client3, request3, response3)
 		newState = append(response.NewState, response2.NewState...)
 	}
 
