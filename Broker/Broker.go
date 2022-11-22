6a92093d4f4d3645bb6006ac2376a73fdfa41981
Mohamed Raid Athamena <jg20659@it075798.wks.bris.ac.uk>
2022-11-22 21:27:27 +0000

    message

--- Broker/Broker.go
+++ Broker/Broker.go
@@ -29,28 +29,33 @@ func (s *Broker) Client(req stubs.Request, res *stubs.Response) (err error) {
 	server := flag.String("server-1-"+strconv.Itoa(x), "44.201.122.214:8030", "IP:port string to connect to as server")
 	server2 := flag.String("server-2-"+strconv.Itoa(x), "54.90.210.50:8030", "IP:port string to connect to as server")
 	server3 := flag.String("server-3-"+strconv.Itoa(x), "18.212.57.66:8030", "IP:port string to connect to as server")
+	server4 := flag.String("server-4-"+strconv.Itoa(x), "35.172.135.168:8030", "IP:port string to connect to as server")
 	x++
 	flag.Parse()
 	client, _ := rpc.Dial("tcp", *server)
 	client2, _ := rpc.Dial("tcp", *server2)
 	client3, _ := rpc.Dial("tcp", *server3)
+	client4, _ := rpc.Dial("tcp", *server4)
 	defer client.Close()
 	defer client2.Close()
 	newState := req.CurrentStates
 
 	for turns := 0; turns < req.P.Turns; turns++ {
-		request := stubs.Request{newState, req.P, 0, "", 3, 0, 1}
-		request2 := stubs.Request{newState, req.P, 0, "", 3, int(req.P.ImageHeight / 3), 2}
-		request3 := stubs.Request{newState, req.P, 0, "", 3, 2 * int(req.P.ImageHeight/3), 3}
+		request := stubs.Request{newState, req.P, 0, "", 4, 0, 1}
+		request2 := stubs.Request{newState, req.P, 0, "", 4, int(req.P.ImageHeight / 4), 2}
+		request3 := stubs.Request{newState, req.P, 0, "", 4, 2 * int(req.P.ImageHeight/4), 3}
+		request4 := stubs.Request{newState, req.P, 0, "", 4, 4 * int(req.P.ImageHeight/4), 4}
 		response := new(stubs.Response)
 		response2 := new(stubs.Response)
 		response3 := new(stubs.Response)
+		response4 := new(stubs.Response)
 		makeCall(client, request, response)
 		makeCall(client2, request2, response2)
 		makeCall(client3, request3, response3)
-		fmt.Println(len(response.NewState), len(response2.NewState), len(response3.NewState))
+		makeCall(client4, request4, response4)
 		newState = append(response.NewState, response2.NewState...)
 		newState = append(newState, response3.NewState...)
+		newState = append(newState, response4.NewState...)
 	}
 
 	res.NewState = newState
