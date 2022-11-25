5ccf0c7b568dffdd8250e4e14c34fde3cd95284b
Mr JP <101270342+HY-IA@users.noreply.github.com>
2022-11-25 20:49:51 +0000

    grh5t

--- Broker/Broker.go
+++ Broker/Broker.go
@@ -169,6 +169,7 @@ func (s *Broker) Client(req stubs.Request, res *stubs.Response) (err error) {
 		mutex.Lock()
 		newState = nil
 		fmt.Println(len(responses[0].NewState), len(responses[1].NewState), len(responses[2].NewState), len(responses[3].NewState))
+		fmt.Println(responses[0].NewState)
 		for i := 0; i < numberOfAWS; i++ {
 
 			newState = append(newState, responses[i].NewState...)
