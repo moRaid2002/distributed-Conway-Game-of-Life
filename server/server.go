1ff5691ff3b4a4896634d465b27667be60944fa4
Mohamed Raid Athamena <jg20659@it075822.wks.bris.ac.uk>
2022-11-28 17:36:51 +0000

    vsnjkvnj

--- server/server.go
+++ server/server.go
@@ -17,7 +17,7 @@ import (
 var x = 0
 
 func SendIp(str string) {
-	server := flag.String("broker"+strconv.Itoa(x), "54.160.190.37:8030", "IP:port string to connect to as server")
+	server := flag.String("broker"+strconv.Itoa(x), "54.196.194.84:8030", "IP:port string to connect to as server")
 	flag.Parse()
 	client, _ := rpc.Dial("tcp", *server)
 	defer client.Close()
